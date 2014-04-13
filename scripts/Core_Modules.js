e2908a77141c6de586509723d2c3e9b4837d9a2e
Vigneshwar Venkatachalapathi <haivicky@gmail.com>
2014-04-13 20:33:56 +0530

    all url retrieved

--- scripts/Core_Modules.js
+++ scripts/Core_Modules.js
@@ -2,5 +2,22 @@ document.getElementById("btn_save").addEventListener("click",RetrievePages);
 
 function RetrievePages(){
 	window.alert("Hi - from inside the function");
+	var fourmTabs = new Array();
+	chrome.tabs.query({}, function (tabs) {
+	    for (var i = 0; i < tabs.length; i++) {
+	        fourmTabs[i] = tabs[i].url;
+	    }
+	    
+	    for (var i = 0; i < fourmTabs.length; i++) {
+	        if (fourmTabs[i] != null)
+	        	{
+	        		console.log(fourmTabs[i]);
+	        		
+	        	}
+	        else {
+	           console.log("??" + i);
+	        }
+    }
+});
 	
 }
