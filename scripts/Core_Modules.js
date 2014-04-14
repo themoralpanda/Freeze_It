b90d6f72662f8b6b5a9bb5292ec9b66fb8517d41
Vigneshwar Venkatachalapathi <haivicky@gmail.com>
2014-04-14 17:35:19 +0530

    Ui_fixed_Working_version_1.0

--- scripts/Core_Modules.js
+++ scripts/Core_Modules.js
@@ -9,18 +9,71 @@ function RetrievePages(){
 	    }
 	    
 	    var links = new String();
-	    for (var i = 0; i < fourmTabs.length; i++) {
+	    var name = document.getElementById('name').value;
+	    //name is the session name
+	    var user = document.getElementById('user').value;
+	    name = String(name);
+	    user = String(user);
+	    links+= "<b> From: </b>  <i>"+user+"</i> <br/>";
+	    links+= "<b>Session name:  <i></b> "+name+" </i></h3><br/>";
+	    links+= "<b>Session date:   <i></b>"+ new Date().toUTCString()+ "</i><br/> <br/><br/>" ;
+	    links+= "<table>"
+	    for (var i = 0, a=1; i < fourmTabs.length-1; i++,a++) {
 	        if (fourmTabs[i] != null)
 	        	{
 	        		console.log(fourmTabs[i]);
+	        		links +="<tr><td>"
+	        		links += a+" - <a href='";
 	        		links += String(fourmTabs[i]);
-	        		links += ""
-	        		
+	        		links += "'><b>"+String(fourmTabs[i])+"</b></a></td></tr><br/>";
 	        	}
 	        else {
 	           console.log("??" + i);
 	        }
-    }
+    	}
+    	links+="</table>"
+
+    console.log(String(links));
+    var email_from = document.getElementById('email_from').value;
+    email_from = String(email_from);
+   
+    var email = document.getElementById('email').value;
+    email = String(email);
+	var pattern = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+    if (pattern.test(email)) {
+	    
+	    var d = new Date();
+	    d = String(d);
+
+	    var subject = "Links browsed by "+user+" on :" + d;
+
+	    $.ajax({
+	  		type: "POST",
+			  url: "https://mandrillapp.com/api/1.0/messages/send.json",
+			  data: {
+			    'key': '_G97b4x1hGzqM7C7NsmuvQ',
+			    'message': {
+			      'from_email': email_from,
+			      'to': [
+			          {
+			            'email': email,
+			            'type': 'to'
+			          }
+			          
+			        ],
+			      'autotext': 'true',
+			      'subject': subject,
+			      'html': links
+			    }
+			  }
+			 }).done(function(response) {
+			   console.log(response); 
+			 });
+
+	} else {
+        alert('Bad email address: ' + email);
+        
+    }	 
 });
 	
 }
